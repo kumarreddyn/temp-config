d1288ed0a16e9ec04afc544d0720e35887d34ca8
Kumar Reddy N <kumarreddyn@outlook.com>
2021-02-03 14:35:31 +0530

    Initial commit

---
+++ src/test/java/kumarreddyn/github/fda/temp/config/TempConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package kumarreddyn.github.fda.temp.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
